de2f9a26320e3541d98d8824c29971ed6ab8de8a
Sarthik <sarthikk2004@gmail.com>
2026-06-26 14:17:20 +0530

    routes correction

--- backend/controller/feedbackController.js
+++ backend/controller/feedbackController.js
@@ -3,13 +3,31 @@ import feedback from "../models/feedback.js";
 export const createFeedback = async (req, res) => {
   try {
     const { name, email, message } = req.body;
-    const newfeedback = await feedback.create({ name, email, message });
+    const userFeedback = await feedback.create({ name, email, message });
 
     res.status(200).json({
       message: "feedback created successfully",
-      data: newfeedback,
+      data: userFeedback,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+export const getFeedback = async (req, res) => {
+  try {
+    const userFeedback = await feedback.find().sort({ createdAt: -1 });
+    res.status(200).json(userFeedback);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteFeedback = async (req, res) => {
+  try {
+    await feedback.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Feedback delete successfully" });
+  } catch (error) {
+    res.status(500).json({ message: message.error });
   }
 };
--- backend/routes/feedbackRoutes.js
+++ backend/routes/feedbackRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { createFeedback } from "../controller/feedbackController.js";
+import {
+  createFeedback,
+  deleteFeedback,
+  getFeedback,
+} from "../controller/feedbackController.js";
 
 const router = express.Router();
 
 router.post("/", createFeedback);
+router.get("/", getFeedback);
+router.delete("/:id", deleteFeedback);
 
 export default router;
